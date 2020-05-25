2e0637593dce64545ddfe2197c929309dae23a98
AakankshaKaskar <akankshakaskar3108@gmail.com>
2020-05-25 14:20:37 -0400

    Initial commit

---
+++ hello-world-rest-api/src/main/java/edu/akslearning/aws/helloworldrestapi/HelloWorldRestApiApplication.java
@@ -0,0 +1,13 @@
+package edu.akslearning.aws.helloworldrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldRestApiApplication.class, args);
+	}
+
+}
