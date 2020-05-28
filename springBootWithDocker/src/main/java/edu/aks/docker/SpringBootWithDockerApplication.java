ca4ed424986ed816a2ea986ed8badc416c8bf258
AakankshaKaskar <akankshakaskar3108@gmail.com>
2020-05-28 01:00:48 -0400

    Initial commmit

---
+++ springBootWithDocker/src/main/java/edu/aks/docker/SpringBootWithDockerApplication.java
@@ -0,0 +1,13 @@
+package edu.aks.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithDockerApplication.class, args);
+	}
+
+}
---
+++ springBootWithDocker/src/test/java/edu/aks/docker/SpringBootWithDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.aks.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
