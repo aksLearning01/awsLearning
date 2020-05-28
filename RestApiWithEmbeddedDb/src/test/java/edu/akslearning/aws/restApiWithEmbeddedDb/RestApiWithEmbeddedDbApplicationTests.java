a61761ab0224c9ef7e8bff8aa6883517698edbc1
AakankshaKaskar <akankshakaskar3108@gmail.com>
2020-05-28 19:43:37 -0400

    Initial Commit

---
+++ RestApiWithEmbeddedDb/src/test/java/edu/akslearning/aws/restApiWithEmbeddedDb/RestApiWithEmbeddedDbApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.akslearning.aws.restApiWithEmbeddedDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWithEmbeddedDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
